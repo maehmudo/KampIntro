59fb43f5d600d1dc68616e8af11717b1395a9eef
mahmu <mahmu@DESKTOP-AAD639O>
2022-05-12 19:51:53 +0300

    12 mayıs 2022

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "AHmEET";
+            int yas = 36;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Maehmudo";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "Kero";
+            kurs2.IzlenmeOrani = 77;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.Egitmen = "Beko";
+            kurs3.IzlenmeOrani = 35;
+
+            //Console.WriteLine(kurs1.KursAdi + " "+ kurs1.Egitmen);
+
+            Kurs[] kurslar = new Kurs[] {kurs1,kurs2,kurs3};
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + " : " + kurs.Egitmen);
+            }
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun(); //class tanımladık
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2};
+            
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+
+                Console.WriteLine(urun.Fiyati);
+
+                Console.WriteLine(urun.Aciklama);
+
+                Console.WriteLine("----------------");
+
+
+            }
+
+            Console.WriteLine("-------Metotlar--------");
+            // instance = class örneği
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+        }
+    }
+}
+
+// Metotlar, bizim için tekrar tekrar kullanılabilirliği sağlayan kod bloklarıdır. 
+//'Dont repeat yourself.'
+// DRY - Clean Code - Best Practice
